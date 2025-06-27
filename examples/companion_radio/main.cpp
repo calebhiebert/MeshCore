cf07ba071dcacd1664c2ff6e79bc2e5d30a5e7e8
Caleb Hiebert <calebkhiebert@gmail.com>
2025-06-27 08:23:03 -0500

    Increase location sharing interval to 60 seconds for improved performance

--- examples/companion_radio/main.cpp
+++ examples/companion_radio/main.cpp
@@ -190,7 +190,7 @@ void setup() {
   sensors.setMeshForLocationSharing(&the_mesh);
   // Force enable location sharing for testing
   the_mesh.setLocationSharingEnabled(true);
-  the_mesh.setLocationSharingInterval(1000 * 10);
+  the_mesh.setLocationSharingInterval(1000 * 60);
   #endif
 
 #ifdef DISPLAY_CLASS
